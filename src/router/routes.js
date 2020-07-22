38ee309751d8b4fcc6be951d6cf423835b427aab
易芳强 <yifangqiang@mbcloud.com>
2020-07-22 09:46:03 +0800

    清理文件

--- src/router/routes.js
+++ src/router/routes.js
@@ -12,21 +12,6 @@ export const routes = [
         component: loadable(() => import('@/pages/auth-wechat'))
     },
     {
-        path: '/orderFillin',
-        inWechat: true,
-        component: loadable(() => import('@/pages/workorder/order-fillin'))
-    },
-    {
-        path: '/orderList',
-        inWechat: true,
-        component: loadable(() => import('@/pages/workorder/order-list'))
-    },
-    {
-        path: '/orderDetail/:id',
-        inWechat: true,
-        component: loadable(() => import('@/pages/workorder/order-detail'))
-    },
-    {
         path: '/notWechat',
         component: loadable(() => import('@/pages/not-wechat'))
     },
